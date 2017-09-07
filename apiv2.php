c5c3d0f8481602d653105debf52a0f7f01236bed
Afif Zafri <zafri_21596@yahoo.com>
2017-09-08 00:37:52 +0800

    added API option for fetching data for all 12 months

--- apiv2.php
+++ apiv2.php
@@ -6,6 +6,7 @@
 	This new version will be able to fetch prayer time data for the whole Year or by each month for chosen Zone
 */
 
+# if month is chosen, then only fetch data for the chosen month
 if(isset($_GET['zon']) && isset($_GET['tahun']) && isset($_GET['bulan']))
 {
 	$kodzon = $_GET['zon']; # store get parameter in variable
@@ -13,7 +14,7 @@ if(isset($_GET['zon']) && isset($_GET['tahun']) && isset($_GET['bulan']))
 	$bulan = $_GET['bulan'];
 
 	$url = "http://www.e-solat.gov.my/web/muatturun.php?zone=".$kodzon."&jenis=year&lang=en&year=".$tahun."&bulan=".$bulan;
-	
+		
 	# use cURL to fetch webpage
     $ch = curl_init(); # initialize curl object
     curl_setopt($ch, CURLOPT_URL, $url); # set url
@@ -28,7 +29,42 @@ if(isset($_GET['zon']) && isset($_GET['tahun']) && isset($_GET['bulan']))
     $patern = '#<table width=\"100%\" cellspacing=\"1\" cellpadding=\"3\" bgcolor=\"\#7C7C7C\"\>([\w\W]*?)</table>#'; 
     preg_match_all($patern, $data, $parsed);  
 
+    # display parse html table
     print_r($parsed[0]);
 }
+
+# if month does not chosen, fetch for all 12 months
+if(isset($_GET['zon']) && isset($_GET['tahun']) && !isset($_GET['bulan']))
+{
+	$kodzon = $_GET['zon']; # store get parameter in variable
+	$tahun = $_GET['tahun'];
+
+	for($i=1;$i<=12;$i++)
+	{
+
+		$url = "http://www.e-solat.gov.my/web/muatturun.php?zone=".$kodzon."&jenis=year&lang=en&year=".$tahun."&bulan=".$i;
+		
+		# use cURL to fetch webpage
+	    $ch = curl_init(); # initialize curl object
+	    curl_setopt($ch, CURLOPT_URL, $url); # set url
+	    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); # receive server response
+	    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0); # do not verify SSL
+	    $data = curl_exec($ch); # execute curl, fetch webpage content
+	    echo curl_error($ch);
+	    $httpstatus = curl_getinfo($ch, CURLINFO_HTTP_CODE); # receive http response status
+	    curl_close($ch);  # close curl
+
+	    # parse the data using regex
+	    $patern = '#<table width=\"100%\" cellspacing=\"1\" cellpadding=\"3\" bgcolor=\"\#7C7C7C\"\>([\w\W]*?)</table>#'; 
+	    preg_match_all($patern, $data, $parsed);  
+
+	    # display parse html table
+	    print_r($parsed[0]);
+	}
+}
+
+#### todo
+# parse each row of the table
+# return JSON
 
 ?>
