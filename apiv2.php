2b08735a604b524b6ca0ae7014cc38db0db71344
Afif Zafri <zafri_21596@yahoo.com>
2017-09-07 23:52:38 +0800

    First commit for API v2

---
+++ apiv2.php
@@ -0,0 +1,9 @@
+<?php 
+
+/*
+	Waktu Solat API v2 created by Afif Zafri
+	XML data are fetch directly from JAKIM e-solat website
+	This new version will be able to fetch prayer time data for the whole Year or by each month for chosen Zone
+*/
+
+?>
