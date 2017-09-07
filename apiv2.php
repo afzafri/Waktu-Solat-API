aa8bef0a963af9d6406cd35097c28b8f4b9cc4b5
Afif Zafri <zafri_21596@yahoo.com>
2017-09-08 00:07:50 +0800

    initial code for fetching the data for a month

--- apiv2.php
+++ apiv2.php
@@ -6,4 +6,24 @@
 	This new version will be able to fetch prayer time data for the whole Year or by each month for chosen Zone
 */
 
+if(isset($_GET['zon']) && isset($_GET['tahun']) && isset($_GET['bulan']))
+{
+	$kodzon = $_GET['zon']; # store get parameter in variable
+	$tahun = $_GET['tahun'];
+	$bulan = $_GET['bulan'];
+
+	$url = "http://www.e-solat.gov.my/web/muatturun.php?zone=".$kodzon."&jenis=year&lang=en&year=".$tahun."&bulan=".$bulan;
+	
+	# use cURL to fetch webpage
+    $ch = curl_init(); # initialize curl object
+    curl_setopt($ch, CURLOPT_URL, $url); # set url
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); # receive server response
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0); # do not verify SSL
+    $data = curl_exec($ch); # execute curl, fetch webpage content
+    echo curl_error($ch);
+    $httpstatus = curl_getinfo($ch, CURLINFO_HTTP_CODE); # receive http response status
+    curl_close($ch);  # close curl
+
+    print_r($data);
+}
 ?>
